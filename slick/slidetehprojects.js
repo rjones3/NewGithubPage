9e7128aa03544e6ea239ee0f07ae2dc737871ed0
Ryan Jones <95ryanjones@gmail.com>
2019-05-01 18:28:06 -0500

    Building new github page

---
+++ slick/slidetehprojects.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){ 
+    $('.project-slider').slick({
+    arrows: false,
+    infinite: true,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+    });
+});
